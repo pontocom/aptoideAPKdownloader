65231a8e718ee63823a9efba483bde754f1e0b24
Carlos Serrao <carlos.j.serrao@gmail.com>
2019-03-11 12:47:41 +0000

    Just added the possibility to use MD5 hashes to identify the different APKs.

--- README.md
+++ README.md
@@ -1,29 +1,38 @@
 # aptoideAPKdownloader
 
-Tool that can be used to download APKs from the [Aptoide](www.aptoide.com) app store. The tool downloads a list of APKs from the store that are recorded on a file.
+Tool that can be used to download APKs from the [Aptoide](www.aptoide.com) app store. The tool can download a list of 
+APKs from the store that are recorded on a file, or it can download directly an APK using its MD5.
 
-On the Aptoide store select the application id (**app_id**) of the APK that you wish to download, that can be found on the "**Download**" button link:
-```
-https://en.aptoide.com/thank-you?app_id=41563327&store_name=sommydany
-```
+On the [Aptoide store](https://en.aptoide.com/) select the application id (MD5) of the APK that you wish to download, 
+that can be found on the end  of the page, under the "**Detailed Information**" link.
 
-You can build an file with a list of app_ids to be downloaded in order for our tool to use!
+You can build an file with a list of app_ids (MD5) to be downloaded in order for our tool to use!
 
 ## Tool usage
 
 In order to use the tool the following command and options can be used:
 
 ```
-python3 aptoideAPKdownloader.py [-h, --help] [-v, --version] [-i, --input] FILE 
+python3 aptoideAPKdownloader.py [-h, --help] [-v, --version] [-f, --file] FILE [-m, --md5] MD5
 ```
 
 **-h, --help**: Displays the usage of the tool (**optional**).
-**-v, --version**: Displays the version of the tool (**optional**).
-**-i, --input**: This parameter is mandatory and is used to pass the file that contains the list of application ids that will be downloaded by the tool (mandatory). The file will have an app_id per line.
 
-**Example**:
+**-v, --version**: Displays the version of the tool (**optional**).
+
+**-f, --file**: This parameter is mandatory and is used to pass the file that contains the list of application ids (MD5) 
+that will be downloaded by the tool. The file will have an app_id (MD5) per line.
+
+**-m, --md5**: Downloads a single APK from Aptoid according to an MD5 app_id.
+
+**Examples**:
 
 ```
-python3 aptoideAPKdownloader.py -i apks.txt
+python3 aptoideAPKdownloader.py -f apks.txt
 ```
 This will download all the APKs, corresponding to the app ids contained in the apks.txt file.
+
+```
+python3 aptoideAPKdownloader.py -m 0bbf5574c82545698f22f5f9210c7571
+```
+This will download a single APK, corresponding to the app id (MD5) selected by the user.
--- aptoideAPKdownload.py
+++ aptoideAPKdownload.py
@@ -5,6 +5,44 @@ from urllib.parse import urlparse
 import os
 import argparse
 
+aptoide_API_endpoint = "http://ws2.aptoide.com/api/7/app/get/apk_md5sum="
+
+
+def download_apk(which_apk):
+    response = requests.get(aptoide_API_endpoint + which_apk)
+
+    jsondata = response.json()
+
+    applicationName = jsondata["nodes"]["meta"]["data"]["name"]
+    applicationPackage = jsondata["nodes"]["meta"]["data"]["package"]
+    appVersion = jsondata["nodes"]["meta"]["data"]["file"]["vername"]
+    appMD5 = jsondata["nodes"]["meta"]["data"]["file"]["md5sum"]
+    appPath = jsondata["nodes"]["meta"]["data"]["file"]["path"]
+
+    print("Getting the following APK => " + applicationName)
+    print(applicationPackage + " (" + appVersion + ") -> " + appMD5)
+    print(appPath)
+
+    # write the file to the filesystem
+    a = urlparse(appPath)
+    filename = os.path.basename(a.path)
+
+    # Streaming, so we can iterate over the response.
+    r = requests.get(appPath, stream=True)
+
+    # Total size in bytes.
+    total_size = int(r.headers.get('content-length', 0));
+    block_size = 1024
+    wrote = 0
+    with open(filename, 'wb') as f:
+        for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB',
+                         unit_scale=True):
+            wrote = wrote + len(data)
+            f.write(data)
+    if total_size != 0 and wrote != total_size:
+        print("ERROR, something went wrong")
+
+
 if __name__=="__main__":
     VERSION = '0.1'
     banner = """
@@ -19,48 +57,18 @@ if __name__=="__main__":
     text = "Tool that downloads APKs from the Aptoide app store"
     parser = argparse.ArgumentParser(description = text)
     parser.add_argument('-v','--version', action='version', version='Aptoide APK Downloader ' + VERSION)
-    parser.add_argument('-i', '--input', help='Input file that contains the identifiers of the Aptoide apps to search and download (default filename is ./apks.txt).', action='store', dest='file', nargs=1, default='teste123')
+    parser.add_argument('-f', '--file', help='Input file that contains the list of MD5 identifiers of the Aptoide apps to search and download (default filename is ./apks.txt).', action='store', dest='file', nargs=1, default='apks.txt')
     parser.add_argument('-m', '--md5', help='The MD5 id of the app to download!', action='store', dest='md5', nargs=1, default='')
     args = parser.parse_args()
 
-    # aptoide_API_endpoint = "http://ws2.aptoide.com/api/7/app/get/app_id="
-    aptoide_API_endpoint = "http://ws2.aptoide.com/api/7/app/get/apk_md5sum="
-
     print(args)
 
+    if args.file == '' and args.md5 == '':
+        print("No arguments were used... please use -h option to display help!")
+        exit(0)
+
     if args.md5:
-        response = requests.get(aptoide_API_endpoint + args.md5[0])
-
-        jsondata = response.json()
-
-        applicationName = jsondata["nodes"]["meta"]["data"]["name"]
-        applicationPackage = jsondata["nodes"]["meta"]["data"]["package"]
-        appVersion = jsondata["nodes"]["meta"]["data"]["file"]["vername"]
-        appMD5 = jsondata["nodes"]["meta"]["data"]["file"]["md5sum"]
-        appPath = jsondata["nodes"]["meta"]["data"]["file"]["path"]
-
-        print("Getting the following APK => " + applicationName)
-        print(applicationPackage + " (" + appVersion + ") -> " + appMD5)
-        print(appPath)
-
-        # write the file to the filesystem
-        a = urlparse(appPath)
-        filename = os.path.basename(a.path)
-
-        # Streaming, so we can iterate over the response.
-        r = requests.get(appPath, stream=True)
-
-        # Total size in bytes.
-        total_size = int(r.headers.get('content-length', 0));
-        block_size = 1024
-        wrote = 0
-        with open(filename, 'wb') as f:
-            for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB',
-                             unit_scale=True):
-                wrote = wrote + len(data)
-                f.write(data)
-        if total_size != 0 and wrote != total_size:
-            print("ERROR, something went wrong")
+        download_apk(args.md5[0])
 
     elif args.file:
         filename = format(args.file[0])
@@ -70,35 +78,4 @@ if __name__=="__main__":
         with open('apks.txt') as f:
             for line in f:
                 id_app = line
-
-                response = requests.get(aptoide_API_endpoint + id_app)
-
-                jsondata = response.json()
-
-                applicationName = jsondata["nodes"]["meta"]["data"]["name"]
-                applicationPackage = jsondata["nodes"]["meta"]["data"]["package"]
-                appVersion = jsondata["nodes"]["meta"]["data"]["file"]["vername"]
-                appMD5 = jsondata["nodes"]["meta"]["data"]["file"]["md5sum"]
-                appPath = jsondata["nodes"]["meta"]["data"]["file"]["path"]
-
-                print("Getting the following APK => " + applicationName)
-                print(applicationPackage + " (" + appVersion + ") -> " + appMD5)
-                print(appPath)
-
-                # write the file to the filesystem
-                a = urlparse(appPath)
-                filename = os.path.basename(a.path)
-
-                # Streaming, so we can iterate over the response.
-                r = requests.get(appPath, stream=True)
-
-                # Total size in bytes.
-                total_size = int(r.headers.get('content-length', 0));
-                block_size = 1024
-                wrote = 0
-                with open( filename, 'wb') as f:
-                    for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size//block_size) , unit='KB', unit_scale=True):
-                        wrote = wrote  + len(data)
-                        f.write(data)
-                if total_size != 0 and wrote != total_size:
-                    print("ERROR, something went wrong")
+                download_apk(id_app)
