ca4c6178d06fcc2095b30c7783dcc7f5bbb0fe2a
Carlos Serrao <carlos.j.serrao@gmail.com>
2019-03-11 09:59:31 +0000

    Just added the possibility to use MD5 hashes to identify the different APKs.

--- aptoideAPKdownload.py
+++ aptoideAPKdownload.py
@@ -20,52 +20,85 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description = text)
     parser.add_argument('-v','--version', action='version', version='Aptoide APK Downloader ' + VERSION)
     parser.add_argument('-i', '--input', help='Input file that contains the identifiers of the Aptoide apps to search and download (default filename is ./apks.txt).', action='store', dest='file', nargs=1, default='teste123')
+    parser.add_argument('-m', '--md5', help='The MD5 id of the app to download!', action='store', dest='md5', nargs=1, default='')
     args = parser.parse_args()
+
+    # aptoide_API_endpoint = "http://ws2.aptoide.com/api/7/app/get/app_id="
+    aptoide_API_endpoint = "http://ws2.aptoide.com/api/7/app/get/apk_md5sum="
 
     print(args)
 
-    filename = ''
-    if len(args.file[0])==1:
-        filename = './apks.txt'
-    else:
+    if args.md5:
+        response = requests.get(aptoide_API_endpoint + args.md5[0])
+
+        jsondata = response.json()
+
+        applicationName = jsondata["nodes"]["meta"]["data"]["name"]
+        applicationPackage = jsondata["nodes"]["meta"]["data"]["package"]
+        appVersion = jsondata["nodes"]["meta"]["data"]["file"]["vername"]
+        appMD5 = jsondata["nodes"]["meta"]["data"]["file"]["md5sum"]
+        appPath = jsondata["nodes"]["meta"]["data"]["file"]["path"]
+
+        print("Getting the following APK => " + applicationName)
+        print(applicationPackage + " (" + appVersion + ") -> " + appMD5)
+        print(appPath)
+
+        # write the file to the filesystem
+        a = urlparse(appPath)
+        filename = os.path.basename(a.path)
+
+        # Streaming, so we can iterate over the response.
+        r = requests.get(appPath, stream=True)
+
+        # Total size in bytes.
+        total_size = int(r.headers.get('content-length', 0));
+        block_size = 1024
+        wrote = 0
+        with open(filename, 'wb') as f:
+            for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB',
+                             unit_scale=True):
+                wrote = wrote + len(data)
+                f.write(data)
+        if total_size != 0 and wrote != total_size:
+            print("ERROR, something went wrong")
+
+    elif args.file:
         filename = format(args.file[0])
 
-    print("Reading APK info from: " + filename)
+        print("Reading APK info from: " + filename)
 
-    aptoide_API_endpoint = "http://ws2.aptoide.com/api/7/app/get/app_id="
+        with open('apks.txt') as f:
+            for line in f:
+                id_app = line
 
-    with open('apks.txt') as f:
-        for line in f:
-            id_app = line
+                response = requests.get(aptoide_API_endpoint + id_app)
 
-            response = requests.get(aptoide_API_endpoint + id_app)
+                jsondata = response.json()
 
-            jsondata = response.json()
+                applicationName = jsondata["nodes"]["meta"]["data"]["name"]
+                applicationPackage = jsondata["nodes"]["meta"]["data"]["package"]
+                appVersion = jsondata["nodes"]["meta"]["data"]["file"]["vername"]
+                appMD5 = jsondata["nodes"]["meta"]["data"]["file"]["md5sum"]
+                appPath = jsondata["nodes"]["meta"]["data"]["file"]["path"]
 
-            applicationName = jsondata["nodes"]["meta"]["data"]["name"]
-            applicationPackage = jsondata["nodes"]["meta"]["data"]["package"]
-            appVersion = jsondata["nodes"]["meta"]["data"]["file"]["vername"]
-            appMD5 = jsondata["nodes"]["meta"]["data"]["file"]["md5sum"]
-            appPath = jsondata["nodes"]["meta"]["data"]["file"]["path"]
+                print("Getting the following APK => " + applicationName)
+                print(applicationPackage + " (" + appVersion + ") -> " + appMD5)
+                print(appPath)
 
-            print("Getting the following APK => " + applicationName)
-            print(applicationPackage + " (" + appVersion + ") -> " + appMD5)
-            print(appPath)
+                # write the file to the filesystem
+                a = urlparse(appPath)
+                filename = os.path.basename(a.path)
 
-            # write the file to the filesystem
-            a = urlparse(appPath)
-            filename = os.path.basename(a.path)
+                # Streaming, so we can iterate over the response.
+                r = requests.get(appPath, stream=True)
 
-            # Streaming, so we can iterate over the response.
-            r = requests.get(appPath, stream=True)
-
-            # Total size in bytes.
-            total_size = int(r.headers.get('content-length', 0)); 
-            block_size = 1024
-            wrote = 0 
-            with open( filename, 'wb') as f:
-                for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size//block_size) , unit='KB', unit_scale=True):
-                    wrote = wrote  + len(data)
-                    f.write(data)
-            if total_size != 0 and wrote != total_size:
-                print("ERROR, something went wrong")  
+                # Total size in bytes.
+                total_size = int(r.headers.get('content-length', 0));
+                block_size = 1024
+                wrote = 0
+                with open( filename, 'wb') as f:
+                    for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size//block_size) , unit='KB', unit_scale=True):
+                        wrote = wrote  + len(data)
+                        f.write(data)
+                if total_size != 0 and wrote != total_size:
+                    print("ERROR, something went wrong")
